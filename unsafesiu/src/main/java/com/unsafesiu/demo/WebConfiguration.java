4c225749b6a8399d5a4bd6a1335c5f239320822b
DM_E91222 <d.montesano@redlink.com.ar>
2023-06-22 10:42:18 -0300

    CORS arreglado

--- unsafesiu/src/main/java/com/unsafesiu/demo/WebConfiguration.java
+++ unsafesiu/src/main/java/com/unsafesiu/demo/WebConfiguration.java
@@ -9,6 +9,10 @@ public class WebConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*");
+    	registry.addMapping("/**")
+		.allowedOrigins("*")
+		.allowedHeaders("*")
+		.allowedMethods("*");
+
     }
 }
